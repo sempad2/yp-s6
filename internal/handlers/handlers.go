ce02522668a23fea0df90cae3f93f7ce5f84441d
SP <sempad@mail.ru>
2026-04-13 13:57:16 +0500

    3rd iteration

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -51,7 +51,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(convertedResult))
 
 }
